809f158d66dcdfab5edda6d2e4bd55d432e2324f
DIVYA ARYA <142824451+aryadivy06@users.noreply.github.com>
2025-09-04 20:12:48 +0530

    Update outliers.py

--- Numpy and Pandas/Mini_project/outliers.py
+++ Numpy and Pandas/Mini_project/outliers.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("data.csv")
-arr = df.select_dtypes(include=[np.number]).to_numpy() 
+df = pd.read_csv("datasetExample.csv")
 
-def outlier_detection(arr):
+def outlier_detection(df):
     outliers = {}
-    for col in range(arr.shape[1]):  
-        data = arr[:, col]           
+    numeric_df = df.select_dtypes(include=[np.number])
+    for col in numeric_df.columns:
+        data = numeric_df[col].dropna() 
         q1, q3 = np.percentile(data, [25, 75])
         IQR = q3 - q1
         lr = q1 - 1.5 * IQR
         ur = q3 + 1.5 * IQR
         
-        # find outliers in this column
+       
         col_outliers = data[(data < lr) | (data > ur)]
-        outliers[f"Column {col}"] = col_outliers
+        outliers[col] = col_outliers.values
     
     return outliers
 
-outliers = outlier_detection(arr)
+
+outliers = outlier_detection(df)
 print(outliers)
